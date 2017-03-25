6b549f86518a9b7ecd0719439a98fd680702931b
hem201197 <hemgalaxy1@gmail.com>
2017-03-26 01:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,6 @@ button.onclick = function () {
     request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     var request = new XMLHttpRequest();
@@ -35,6 +33,9 @@ submit.onclick = function () {
        }
     }
     };
+    
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
     request.open('GET','http://hem201197.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
