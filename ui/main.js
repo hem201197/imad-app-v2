0d20af4c5d3014a13c7bfbaa209cd423cbff6f30
hem201197 <hemgalaxy1@gmail.com>
2017-03-25 22:02:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 var button = document.getElementById('counter');
-button.onclick = function(){
+button.onclick = function (){
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200){
-            var counter = request.reponseText;
+            var counter = request.responseText;
                 var span = document.getElementById('count');
     span.innerHTML = counter.toString();
         }
