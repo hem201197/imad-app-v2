2c2d9e14a8bd1a7df8075de25a448e622bb72b5d
hem201197 <hemgalaxy1@gmail.com>
2017-03-26 00:54:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,6 @@ submit.onclick = function () {
         
         list +- '<li>' + names[i] + '</li>';
     }
-    var ui = document.getElementById('namelist');
+    var ul = document.getElementById('namelist');
     ul.innerHTML = list;
 };
