20cf25e172afe8b64252c6581a25034bc2cbe773
hem201197 <hemgalaxy1@gmail.com>
2017-03-25 18:05:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById(main-text);
 element.innerhtml= 'New Value';
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft='100px';
 };
