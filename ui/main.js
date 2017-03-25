bc3559a4bc28d7e4b514d948289cd160c85820a4
hem201197 <hemgalaxy1@gmail.com>
2017-03-26 00:40:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,18 @@ button.onclick = function () {
     request.open('GET','http://hem201197.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for (var i=0;i< names.length; i++) {
+        
+        list +- '<liv>' + names[i] + '</li>';
+    }
+    var ui = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
